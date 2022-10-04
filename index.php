ca246d04bfbac05bcfdbdc37ae354632f3028e52
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-04 18:04:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
   <tbody>
     <?php
 $servername = "localhost";
-$username = "projecto_homework3";
-$password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$username = "jackstra_wp1";
+$password = "(0TwrRD@gUlB";
+$dbname = "jackstra_Homework3";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
