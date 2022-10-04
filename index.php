b18ad71acf19e4afa566fd982a0bb375bea4d66f
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-04 18:09:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+    
 $sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
 
