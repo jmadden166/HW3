b260b4921a832f0e0a3771bbc33cf64bd29b0cb6
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-05 12:57:16 -0500

    Create page7.php

---
+++ page7.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<?php
+$servername = "localhost";
+$username = "jackstra_Homework3";
+$password = "+JMx3n7)k+!H";
+$dbname = "jackstra_Homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+  ?>
+
+  <?php  
+ 
+$sql1 = "select StudentID, Name from Student where Name='Emily%'";
+$result2 = $conn->query($sql1);
+
+if ($result2->num_rows > 0) {
+  // output data of each row
+  while($row = $result2->fetch_assoc()) {
+    ?>
+     <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["Name"]?></h5>
+      <p class="card-text"><ul>
+<?php
+  
+    echo "<li>- Student ID: " . $row["StudentID"]. " - Favorite Class ID: " . $row["FavoriteClassID"]. " - Favorite Teacher ID: " . $row["ID"]. "</li>";
+   ?>
+      <br>
+         </ul></p>
+  </div>
+    </div>
+  <?php  
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</html>
