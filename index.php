86425e4c7f84c6d8f00c3a639f91d830ea7321af
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-04 19:54:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Instructors</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+</head>
   <body>
     <h1>Instructors</h1>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-    </tr>
-  </thead>
-  <tbody>
+<div class="card-group">
     <?php
 $servername = "localhost";
 $username = "jackstra_Homework3";
@@ -35,19 +28,26 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-
+    ?>
+     <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?=$row["instructor_name"]?></h5>
+      <p class="card-text"><ul>
+<?php
   
     echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " - Class: " . $row["Class"]. " - Number of Classes Taught: " .  $row["Number of Classes"]. "<br>";
- 
-   
+   ?>
+         </ul></p>
+  </div>
+    </div>
+  <?php  
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+  </card-group>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
