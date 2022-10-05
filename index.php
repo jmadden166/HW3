b9f8be0d93431b7e3a2bf907d2c276635462324a
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-05 13:01:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -61,8 +61,10 @@ $conn->close();
   <input type="text" id="1GPA" name="1GPA">
   <input type="submit" value="Submit">
 </form>    
-   <a href="page5.php">Go to Page 2</a> 
+  <p>
+   <a href="page5.php">Go to Page 2</a> <br>
   <a href="page7.php">Look for a Student named Emily</a> 
+  </p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
