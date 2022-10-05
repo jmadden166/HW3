a5b7d1ffb52a5eb8cfdb58db77c2d698d091abd2
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-10-05 09:33:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if ($result->num_rows > 0) {
 <?php
   
     
-    echo "<li>" . $row["ID"]. " - Name: " . $row["Name"]. " - Class: " . $row["Class"]. " - Number of Classes Taught: " .  $row["Number of Classes"]. "</li>";
+    echo "<li>" . $row["ID"]. "</li>- Name: " . $row["Name"]. " - Class: " . $row["Class"]. " - Number of Classes Taught: " .  $row["Number of Classes"]. "</li>";
    ?>
          </ul></p>
   </div>
